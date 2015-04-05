40847ac6caaa33ec4a079cd811782a9831e50beb
avivch <aaa.charikar@gmail.com>
2015-04-05 17:26:29 +0300

    Each path can only have one relation

--- RelPathsChooser/src/relations/RelPathsChooser.java
+++ RelPathsChooser/src/relations/RelPathsChooser.java
@@ -1,12 +1,12 @@
 package relations;
 
 import java.io.*;
-import java.util.*;
 
 import parser.POSRelPath;
 
 public class RelPathsChooser {
 	private RelAndFile[] input;
+	private RelPathsTable relPathsTable;
 	private double selectionPercent;
 	private String outputFile;
 	
@@ -18,53 +18,27 @@ public class RelPathsChooser {
 	
 	public void run() {
 		try {
-			boolean first = true;
-			PrintWriter writer = new PrintWriter(outputFile);
-			for (RelAndFile relAndFile : input) {
-				if (!first)
-					writer.println();
-				else
-					first = false;
-				writer.println(relAndFile.rel);
-				for (POSRelPath path : choosePaths(relAndFile.file))
-					writer.println(path.toString());
-			}
-			writer.close();
+			relPathsTable = new RelPathsTable();
+			readPaths();
+			relPathsTable.chooseBestRelPaths(selectionPercent);
+			relPathsTable.outputRelPathsTable(outputFile);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private ArrayList<POSRelPath> choosePaths(String file) throws IOException {
-		String line;
-		int countAll = 0;
-		BufferedReader reader = new BufferedReader(new FileReader(file));
-		while ((line = reader.readLine()) != null) {
-			int count = Integer.parseInt(line.split("\t")[1]);
-			countAll += count;
-		}
-		int pathsCountNo = (int)Math.ceil(selectionPercent / 100 * countAll);
-		reader.close();
-		
-		ArrayList<POSRelPath> paths = new ArrayList<POSRelPath>();
-		int pathsChosen = 0;
-		int lastCount = 0;
-		reader = new BufferedReader(new FileReader(file));
-		while ((line = reader.readLine()) != null) {
-			String[] lineParts = line.split("\t");
-			POSRelPath path = new POSRelPath(lineParts[0]);
-			int count = Integer.parseInt(lineParts[1]);
-			
-			if ((pathsChosen < pathsCountNo) || (count == lastCount)) {
-				paths.add(path);
-				pathsChosen += count;
-				lastCount = count;
+	private void readPaths() throws IOException {
+		for (RelAndFile relAndFile : input) {
+			BufferedReader reader = new BufferedReader(new FileReader(relAndFile.file));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] lineParts = line.split("\t");
+				POSRelPath path = new POSRelPath(lineParts[0]);
+				int count = Integer.parseInt(lineParts[1]);
+				relPathsTable.addRelPath(path, relAndFile.rel, count);
 			}
-			else
-				break;
+			reader.close();
 		}
-		reader.close();
-		return paths;
 	}
 	
 	public static class RelAndFile {
---
+++ RelPathsChooser/src/relations/RelPathsTable.java
@@ -0,0 +1,75 @@
+package relations;
+
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+import parser.POSRelPath;
+
+public class RelPathsTable {
+	private HashMap<POSRelPath, RelationAndCount> paths;
+	private int countAll;
+	
+	public RelPathsTable() {
+		paths = new HashMap<POSRelPath, RelationAndCount>();
+		countAll = 0;
+	}
+	
+	public void addRelPath(POSRelPath relPath, String relation, int count) {
+		RelationAndCount relationAndCount = paths.get(relPath);
+		if (relationAndCount == null) {
+			countAll += count;
+			paths.put(relPath, new RelationAndCount(relation, count));
+		}
+		else if (count > relationAndCount.count) {
+			countAll += count - relationAndCount.count;
+			relationAndCount.relation = relation;
+			relationAndCount.count = count;
+		}
+	}
+	
+	public void chooseBestRelPaths(double selectionPercent) {
+		SortedSet<Entry<POSRelPath, RelationAndCount>> sortedPaths = new TreeSet<Entry<POSRelPath, RelationAndCount>>(new PathsComparator());
+		sortedPaths.addAll(paths.entrySet());
+		HashMap<POSRelPath, RelationAndCount> bestPaths = new HashMap<POSRelPath, RelationAndCount>();
+		int pathsCountNo = (int)Math.ceil(selectionPercent / 100 * countAll);
+		int pathsChosen = 0;
+		int lastCount = 0;
+		for (Entry<POSRelPath, RelationAndCount> entry : sortedPaths) {
+			int count = entry.getValue().count;
+			if ((pathsChosen < pathsCountNo) || (count == lastCount)) {
+				bestPaths.put(entry.getKey(), entry.getValue());
+				pathsChosen += count;
+				lastCount = count;
+			}
+			else
+				break;
+		}
+		paths = bestPaths;
+	}
+	
+	public void outputRelPathsTable(String outputFile) throws IOException {
+		PrintWriter writer = new PrintWriter(outputFile);
+		for (Entry<POSRelPath, RelationAndCount> entry : paths.entrySet())
+			writer.println(entry.getKey().toString() + "\t" + entry.getValue().relation);
+		writer.close();
+	}
+	
+	private static class RelationAndCount {
+		public String relation;
+		public int count;
+		
+		public RelationAndCount(String relation, int count) {
+			this.relation = relation;
+			this.count = count;
+		}
+	}
+	
+	private static class PathsComparator implements Comparator<Entry<POSRelPath, RelationAndCount>> {
+		@Override
+		public int compare(Entry<POSRelPath, RelationAndCount> o1, Entry<POSRelPath, RelationAndCount> o2) {
+			int res = o2.getValue().count - o1.getValue().count;
+			return (res < 0) ? -1 : 1;
+		}
+	}
+}
